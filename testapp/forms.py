db6c2f376bc351e9c44f2d53c1d60dc51f989019
Mohammad Irshad <awairit2k20@gmail.com>
2026-01-10 19:14:14 +0530

    Form Validation :--> Explicitly by the programmer by using 'clean methods'

--- testapp/forms.py
+++ testapp/forms.py
@@ -1,6 +1,38 @@
+# from django import forms
+# class FeedBackForm(forms.Form):
+#     name = forms.CharField()
+#     rollno = forms.IntegerField()
+#     email = forms.EmailField()
+#     feedback = forms.CharField(widget=forms.Textarea)
+
+#Form Validation :--> Explicitly by the programmer by using 'clean methods'
+# Case - 2
+
 from django import forms
 class FeedBackForm(forms.Form):
     name = forms.CharField()
     rollno = forms.IntegerField()
     email = forms.EmailField()
     feedback = forms.CharField(widget=forms.Textarea)
+
+    def clean_name(self):
+        print('Validating name field')
+        inputname = self.cleaned_data['name']
+        if len(inputname) < 4:
+            raise forms.ValidationError('The minimum number of character in the name field should be 4')
+        return inputname
+
+    def clean_rollno(self):
+        print('Validating rollno field')
+        inputrollno = self.cleaned_data['rollno']
+        return inputrollno
+
+    def clean_email(self):
+        print('Validating email field')
+        inputemail = self.cleaned_data['email']
+        return inputemail
+
+    def clean_feedback(self):
+        print('Validating feedback field')
+        inputfeedback = self.cleaned_data['feedback']
+        return inputfeedback
--- testapp/views.py
+++ testapp/views.py
@@ -1,8 +1,33 @@
+# from django.shortcuts import render
+# from testapp.forms import *
+#
+# # Create your views here.
+# def feedback_view(request):
+#     submitted = False
+#     name = ''
+#     if request.method == 'POST':
+#         form = FeedBackForm(request.POST)
+#         if form.is_valid():
+#             print('Form validation success and printing feedback information')
+#             print('*'*50)
+#             print('Name:',form.cleaned_data['name'])
+#             print('RollNo:',form.cleaned_data['rollno'])
+#             print('Email ID:',form.cleaned_data['email'])
+#             print('Feedback:',form.cleaned_data['feedback'])
+#             submitted = True
+#             name = form.cleaned_data['name']
+#     form = FeedBackForm()
+#     return render(request,'testapp/feedback.html',
+# 	{'form':form,'submitted':submitted,'name':name})
+
+#Form Validation :--> Explicitly by the programmer by using 'clean methods'
+
 from django.shortcuts import render
 from testapp.forms import *
 
 # Create your views here.
 def feedback_view(request):
+    form = FeedBackForm() # ------------------------------------>  # Case - 2
     submitted = False
     name = ''
     if request.method == 'POST':
@@ -16,6 +41,9 @@ def feedback_view(request):
             print('Feedback:',form.cleaned_data['feedback'])
             submitted = True
             name = form.cleaned_data['name']
-    form = FeedBackForm()
+        else:                                                       # Case - 2:
+            print('Form validation failed try properly')            # Case - 2:
+            print('*'*50)
+                                                                    # Case - 2:empty form should be first
     return render(request,'testapp/feedback.html',
 	{'form':form,'submitted':submitted,'name':name})
