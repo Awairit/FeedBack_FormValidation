7b0575e6e2018278a695f9cb55032e3b9889a9f8
Mohammad Irshad <awairit2k20@gmail.com>
2026-01-19 16:50:55 +0530

    All code without Commenting

--- testapp/forms.py
+++ testapp/forms.py
@@ -1,156 +1,156 @@
-# from django import forms
-# class FeedBackForm(forms.Form):
-#     name = forms.CharField()
-#     rollno = forms.IntegerField()
-#     email = forms.EmailField()
-#     feedback = forms.CharField(widget=forms.Textarea)
-#
-# # 1) Form Validation :--> Explicitly by the programmer by using 'clean methods'
-# # Case - 2
-#
-# from django import forms
-# class FeedBackForm(forms.Form):
-#     name = forms.CharField()
-#     rollno = forms.IntegerField()
-#     email = forms.EmailField()
-#     feedback = forms.CharField(widget=forms.Textarea)
-#
-#     def clean_name(self):
-#         print('Validating name field')
-#         inputname = self.cleaned_data['name']
-#         if len(inputname) < 4:
-#             raise forms.ValidationError('The minimum number of character in the name field should be 4')
-#         return inputname
-#
-#     def clean_rollno(self):
-#         print('Validating rollno field')
-#         inputrollno = self.cleaned_data['rollno']
-#         return inputrollno
-#
-#     def clean_email(self):
-#         print('Validating email field')
-#         inputemail = self.cleaned_data['email']
-#         return inputemail
-#
-#     def clean_feedback(self):
-#         print('Validating feedback field')
-#         inputfeedback = self.cleaned_data['feedback']
-#         return inputfeedback
-#
-# #=======================================================================================================================
-# # 2). Form Validation :--> By using in-built validators:
-# # Case - 3:
-#
-# from django.core import validators
-#     validators=[validators.MaxLengthValidator(40),validators.MinLengthValidator(10)]
-#
-# from django.core import validators
-# from django import forms
-# class FeedBackForm(forms.Form):
-#     name = forms.CharField()
-#     rollno = forms.IntegerField()
-#     email = forms.EmailField()
-#     feedback = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(40), validators.MinLengthValidator(10)]) #--> Case - 3: Validating by inbuilt Validator(from django.core import Validator)
-#
-# #=======================================================================================================================
-# #Case - 4:
-# #How to implement custom validators by using validators parameter?
-# #The name should be starts with 's' or 'S'
-#
-# from django.core import validators
-# from django import forms
-#
-# def starts_with_s(value):
-#     print('starts_with_s function execution')
-#     if value[0].lower() != 's':
-#         raise forms.ValidationError('Name should be starts with s or S')
-#
-# class FeedBackForm(forms.Form):
-#     name = forms.CharField(validators=[starts_with_s])
-#     rollno = forms.IntegerField()
-#     email = forms.EmailField()
-#     feedback = forms.CharField(widget=forms.Textarea)
-#
-# #----------------------------------------------------------------------------
-#
-# #The name should be starts with 's' or 'S'
-# # mail should contain @gmail.com
-#
-# from django.core import validators
-# from django import forms
-#
-# def starts_with_s(value):
-#     print('starts_with_s function execution')
-#     if value[0].lower() != 's':
-#         raise forms.ValidationError('Name should be starts with s or S')
-#
-# def gmail_validator(value):
-#     print('Checking for gmail validation')
-#     if value[-10:] != '@gmail.com':
-#         raise forms.ValidationError('Mail extension should be gmail')
-#
-# class FeedBackForm(forms.Form):
-#     name = forms.CharField(validators=[starts_with_s])
-#     rollno = forms.IntegerField()
-#     email = forms.EmailField(validators=[gmail_validator])
-#     feedback = forms.CharField(widget=forms.Textarea)
-#
-#
-# #=======================================================================================================================
-#
-# # Case - 5:
-# # Validation of total form directly by using single clean() method
-#
-# from django.core import validators
-# from django import forms
-#
-# class FeedBackForm(forms.Form):
-#     name = forms.CharField()
-#     rollno = forms.IntegerField()
-#     email = forms.EmailField()
-#     feedback = forms.CharField(widget=forms.Textarea)
-#
-#     def clean(self):
-#         print('Total form validation.....')
-#         total_cleaned_data = super().clean()
-#         print('Validating Name')
-#         inputname = total_cleaned_data['name']
-#         if inputname[0].lower() != 's':
-#             raise forms.ValidationError('Name should starts with s')
-#         print('Validating Rollno')
-#         inputrollno = total_cleaned_data['rollno']
-#         if inputrollno <= 0:
-#             raise forms.ValidationError('Rollno should be > 0')
-#         print('Validating Email')
-#         inputemail = total_cleaned_data['email']
-#         if inputemail[-10:] != '@gmail.com':
-#             raise forms.ValidationError('Email extension should be gmail')
-#
-# # =======================================================================================================================
-#
-# # Case - 6:
-# # How to check original pwd and re-entered pwd are same or not?
-#
-# from django.core import validators
-# from django import forms
-#
-# class FeedBackForm(forms.Form):
-#     name = forms.CharField()
-#     password = forms.CharField(label='Enter Password', widget=forms.PasswordInput) # widget=forms.PasswordInput: secure the password characters
-#     rpassword = forms.CharField(label='Password(Again)', widget=forms.PasswordInput)
-#     email = forms.EmailField()
-#
-#     def clean(self):
-#         cleaned_data = super().clean() # Django removes a field from cleaned_data if that field is invalid or empty.
-#         pwd = cleaned_data.get('password') # ALWAYS use .get() inside clean(): Returns value if key exists . Returns None if key does NOT exist .Does NOT crash
-#         rpwd = cleaned_data.get('rpassword')
-#
-#         if pwd and rpwd and pwd != rpwd:
-#             raise forms.ValidationError('Both passwords must be same.....')
-#
-#         return cleaned_data
-#
-# # ===================================================================================================================
+from django import forms
+class FeedBackForm(forms.Form):
+    name = forms.CharField()
+    rollno = forms.IntegerField()
+    email = forms.EmailField()
+    feedback = forms.CharField(widget=forms.Textarea)
+
+# 1) Form Validation :--> Explicitly by the programmer by using 'clean methods'
+# Case - 2
+
+from django import forms
+class FeedBackForm(forms.Form):
+    name = forms.CharField()
+    rollno = forms.IntegerField()
+    email = forms.EmailField()
+    feedback = forms.CharField(widget=forms.Textarea)
+
+    def clean_name(self):
+        print('Validating name field')
+        inputname = self.cleaned_data['name']
+        if len(inputname) < 4:
+            raise forms.ValidationError('The minimum number of character in the name field should be 4')
+        return inputname
+
+    def clean_rollno(self):
+        print('Validating rollno field')
+        inputrollno = self.cleaned_data['rollno']
+        return inputrollno
+
+    def clean_email(self):
+        print('Validating email field')
+        inputemail = self.cleaned_data['email']
+        return inputemail
+
+    def clean_feedback(self):
+        print('Validating feedback field')
+        inputfeedback = self.cleaned_data['feedback']
+        return inputfeedback
+
+#=======================================================================================================================
+# 2). Form Validation :--> By using in-built validators:
+# Case - 3:
+
+from django.core import validators
+    validators=[validators.MaxLengthValidator(40),validators.MinLengthValidator(10)]
+
+from django.core import validators
+from django import forms
+class FeedBackForm(forms.Form):
+    name = forms.CharField()
+    rollno = forms.IntegerField()
+    email = forms.EmailField()
+    feedback = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(40), validators.MinLengthValidator(10)]) #--> Case - 3: Validating by inbuilt Validator(from django.core import Validator)
+
+#=======================================================================================================================
+#Case - 4:
+#How to implement custom validators by using validators parameter?
+#The name should be starts with 's' or 'S'
+
+from django.core import validators
+from django import forms
+
+def starts_with_s(value):
+    print('starts_with_s function execution')
+    if value[0].lower() != 's':
+        raise forms.ValidationError('Name should be starts with s or S')
+
+class FeedBackForm(forms.Form):
+    name = forms.CharField(validators=[starts_with_s])
+    rollno = forms.IntegerField()
+    email = forms.EmailField()
+    feedback = forms.CharField(widget=forms.Textarea)
+
+#----------------------------------------------------------------------------
+
+#The name should be starts with 's' or 'S'
+# mail should contain @gmail.com
+
+from django.core import validators
+from django import forms
+
+def starts_with_s(value):
+    print('starts_with_s function execution')
+    if value[0].lower() != 's':
+        raise forms.ValidationError('Name should be starts with s or S')
+
+def gmail_validator(value):
+    print('Checking for gmail validation')
+    if value[-10:] != '@gmail.com':
+        raise forms.ValidationError('Mail extension should be gmail')
+
+class FeedBackForm(forms.Form):
+    name = forms.CharField(validators=[starts_with_s])
+    rollno = forms.IntegerField()
+    email = forms.EmailField(validators=[gmail_validator])
+    feedback = forms.CharField(widget=forms.Textarea)
+
+
+#=======================================================================================================================
+
+# Case - 5:
+# Validation of total form directly by using single clean() method
+
+from django.core import validators
+from django import forms
+
+class FeedBackForm(forms.Form):
+    name = forms.CharField()
+    rollno = forms.IntegerField()
+    email = forms.EmailField()
+    feedback = forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        print('Total form validation.....')
+        total_cleaned_data = super().clean()
+        print('Validating Name')
+        inputname = total_cleaned_data['name']
+        if inputname[0].lower() != 's':
+            raise forms.ValidationError('Name should starts with s')
+        print('Validating Rollno')
+        inputrollno = total_cleaned_data['rollno']
+        if inputrollno <= 0:
+            raise forms.ValidationError('Rollno should be > 0')
+        print('Validating Email')
+        inputemail = total_cleaned_data['email']
+        if inputemail[-10:] != '@gmail.com':
+            raise forms.ValidationError('Email extension should be gmail')
+
+# =======================================================================================================================
+
+# Case - 6:
+# How to check original pwd and re-entered pwd are same or not?
+
+from django.core import validators
+from django import forms
+
+class FeedBackForm(forms.Form):
+    name = forms.CharField()
+    password = forms.CharField(label='Enter Password', widget=forms.PasswordInput) # widget=forms.PasswordInput: secure the password characters
+    rpassword = forms.CharField(label='Password(Again)', widget=forms.PasswordInput)
+    email = forms.EmailField()
+
+    def clean(self):
+        cleaned_data = super().clean() # Django removes a field from cleaned_data if that field is invalid or empty.
+        pwd = cleaned_data.get('password') # ALWAYS use .get() inside clean(): Returns value if key exists . Returns None if key does NOT exist .Does NOT crash
+        rpwd = cleaned_data.get('rpassword')
+
+        if pwd and rpwd and pwd != rpwd:
+            raise forms.ValidationError('Both passwords must be same.....')
+
+        return cleaned_data
+
+# ===================================================================================================================
 
 """How to prevent request from BOT:
 ----------------------------------------------------
--- testapp/views.py
+++ testapp/views.py
@@ -1,112 +1,112 @@
-# # Case - 1: Simple FeedBack Form
-#
-# from django.shortcuts import render
-# from testapp.forms import *
-#
-# # Create your views here.
-# def feedback_view(request):
-#     submitted = False
-#     name = ''
-#     if request.method == 'POST':
-#         form = FeedBackForm(request.POST)
-#         if form.is_valid():
-#             print('Form validation success and printing feedback information')
-#             print('*'*50)
-#             print('Name:',form.cleaned_data['name'])
-#             print('RollNo:',form.cleaned_data['rollno'])
-#             print('Email ID:',form.cleaned_data['email'])
-#             print('Feedback:',form.cleaned_data['feedback'])
-#             submitted = True
-#             name = form.cleaned_data['name']
-#     form = FeedBackForm()
-#     return render(request,'testapp/feedback.html',
-# 	{'form':form,'submitted':submitted,'name':name})
-#
-# # =======================================================================================================================
-#
-# # Case - 2:
-# # 1) Form Validation :--> Explicitly by the programmer by using 'clean methods'
-#
-# from django.shortcuts import render
-# from testapp.forms import *
-#
-# # Create your views here.
-# def feedback_view(request):
-#     form = FeedBackForm() # ------------------------------------>  # Case - 2
-#     submitted = False
-#     name = ''
-#     if request.method == 'POST':
-#         form = FeedBackForm(request.POST)
-#         if form.is_valid():
-#             print('Form validation success and printing feedback information')
-#             print('*'*50)
-#             print('Name:',form.cleaned_data['name'])
-#             print('RollNo:',form.cleaned_data['rollno'])
-#             print('Email ID:',form.cleaned_data['email'])
-#             print('Feedback:',form.cleaned_data['feedback'])
-#             submitted = True
-#             name = form.cleaned_data['name']
-#         else:                                                       # Case - 2:
-#             print('Form validation failed try properly')            # Case - 2:
-#             print('*'*50)
-#                                                                     # Case - 2:empty form should be first
-#     return render(request,'testapp/feedback.html',
-# 	{'form':form,'submitted':submitted,'name':name})
-#
-# # =======================================================================================================================
-#
-# # Case - 3, 4, 5:
-# # 2). Form Validation :--> By using in-built validators:
-#
-# from django.shortcuts import render
-# from testapp.forms import *
-#
-# def feedback_view(request):
-#     form = FeedBackForm()
-#     submitted = False
-#     name = ''
-#     if request.method == 'POST':
-#         form = FeedBackForm(request.POST)
-#         if form.is_valid():
-#             print('Form validation success and printing feedback information')
-#             print('*'*50)
-#             print('Name:',form.cleaned_data['name'])
-#             print('RollNo:',form.cleaned_data['rollno'])
-#             print('Email ID:',form.cleaned_data['email'])
-#             print('Feedback:',form.cleaned_data['feedback'])
-#             submitted = True
-#             name = form.cleaned_data['name']
-#     return render(request,'testapp/feedback.html',
-# 	{'form':form,'submitted':submitted,'name':name})
-#
+# Case - 1: Simple FeedBack Form
+
+from django.shortcuts import render
+from testapp.forms import *
+
+# Create your views here.
+def feedback_view(request):
+    submitted = False
+    name = ''
+    if request.method == 'POST':
+        form = FeedBackForm(request.POST)
+        if form.is_valid():
+            print('Form validation success and printing feedback information')
+            print('*'*50)
+            print('Name:',form.cleaned_data['name'])
+            print('RollNo:',form.cleaned_data['rollno'])
+            print('Email ID:',form.cleaned_data['email'])
+            print('Feedback:',form.cleaned_data['feedback'])
+            submitted = True
+            name = form.cleaned_data['name']
+    form = FeedBackForm()
+    return render(request,'testapp/feedback.html',
+	{'form':form,'submitted':submitted,'name':name})
+
 # =======================================================================================================================
-#
-# # Case - 6:
-# # How to check original pwd and re-entered pwd are same or not?
-#
-# from django.shortcuts import render
-# from testapp.forms import *
-#
-# def feedback_view(request):
-#     form = FeedBackForm()
-#     submitted = False
-#     name = ''
-#     if request.method == 'POST':
-#         form = FeedBackForm(request.POST)
-#         if form.is_valid():
-#             print('Form validation success and printing feedback information')
-#             print('*'*50)
-#             print('Name:',form.cleaned_data['name'])
-#             print('Password:',form.cleaned_data['password'])
-#             print('Re-Entered Password:', form.cleaned_data['rpassword'])
-#             print('Email ID:',form.cleaned_data['email'])
-#
-#             submitted = True
-#             name = form.cleaned_data['name']
-#     return render(request,'testapp/feedback.html',
-# 	{'form':form,'submitted':submitted,'name':name})
-#
-# # =======================================================================================================================
+
+# Case - 2:
+# 1) Form Validation :--> Explicitly by the programmer by using 'clean methods'
+
+from django.shortcuts import render
+from testapp.forms import *
+
+# Create your views here.
+def feedback_view(request):
+    form = FeedBackForm() # ------------------------------------>  # Case - 2
+    submitted = False
+    name = ''
+    if request.method == 'POST':
+        form = FeedBackForm(request.POST)
+        if form.is_valid():
+            print('Form validation success and printing feedback information')
+            print('*'*50)
+            print('Name:',form.cleaned_data['name'])
+            print('RollNo:',form.cleaned_data['rollno'])
+            print('Email ID:',form.cleaned_data['email'])
+            print('Feedback:',form.cleaned_data['feedback'])
+            submitted = True
+            name = form.cleaned_data['name']
+        else:                                                       # Case - 2:
+            print('Form validation failed try properly')            # Case - 2:
+            print('*'*50)
+                                                                    # Case - 2:empty form should be first
+    return render(request,'testapp/feedback.html',
+	{'form':form,'submitted':submitted,'name':name})
+
+# =======================================================================================================================
+
+# Case - 3, 4, 5:
+# 2). Form Validation :--> By using in-built validators:
+
+from django.shortcuts import render
+from testapp.forms import *
+
+def feedback_view(request):
+    form = FeedBackForm()
+    submitted = False
+    name = ''
+    if request.method == 'POST':
+        form = FeedBackForm(request.POST)
+        if form.is_valid():
+            print('Form validation success and printing feedback information')
+            print('*'*50)
+            print('Name:',form.cleaned_data['name'])
+            print('RollNo:',form.cleaned_data['rollno'])
+            print('Email ID:',form.cleaned_data['email'])
+            print('Feedback:',form.cleaned_data['feedback'])
+            submitted = True
+            name = form.cleaned_data['name']
+    return render(request,'testapp/feedback.html',
+	{'form':form,'submitted':submitted,'name':name})
+
+=======================================================================================================================
+
+# Case - 6:
+# How to check original pwd and re-entered pwd are same or not?
+
+from django.shortcuts import render
+from testapp.forms import *
+
+def feedback_view(request):
+    form = FeedBackForm()
+    submitted = False
+    name = ''
+    if request.method == 'POST':
+        form = FeedBackForm(request.POST)
+        if form.is_valid():
+            print('Form validation success and printing feedback information')
+            print('*'*50)
+            print('Name:',form.cleaned_data['name'])
+            print('Password:',form.cleaned_data['password'])
+            print('Re-Entered Password:', form.cleaned_data['rpassword'])
+            print('Email ID:',form.cleaned_data['email'])
+
+            submitted = True
+            name = form.cleaned_data['name']
+    return render(request,'testapp/feedback.html',
+	{'form':form,'submitted':submitted,'name':name})
+
+# =======================================================================================================================
 
 # Case - 3, 4, 5, 7:
 # Bot Handling
